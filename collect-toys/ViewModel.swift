9236b02dd6d05ab1104ef3c700518590df18736e
lsaganski-really <lsaganski@really.com>
2024-07-09 18:46:42 -0300

    Added Viewmodel and tests

---
+++ collect-toys/ViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  ViewModel.swift
+//  collect-toys
+//
+//  Created by Leonardo Saganski on 09/07/24.
+//
+
+import Foundation
+
+class ViewModel {
+    func sum(a: Int, b: Int) -> Int {
+        return a + b
+    }
+}
